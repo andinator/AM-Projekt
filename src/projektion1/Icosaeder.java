0042acbd659eeb7402bb0d5ebaa5744e91d889bf
Andi <Andi@Andi-PC>
2015-04-09 20:13:11 +0200

    Habe Punkte vom Ikosaeder verbunden. Ikosaeder ist fertig!
    Nextes: Checkboxen fertig machen + Dodekaeder

--- src/projektion1/Icosaeder.java
+++ src/projektion1/Icosaeder.java
@@ -10,44 +10,57 @@ public class Icosaeder extends Koerper {
 		super(new ArrayList<Punkt>(), new ArrayList<Kante>());
 
 		eckpunkt = new Punkt[]{
-		new Punkt(0, 0, -5/Math.sqrt(50-10*Math.sqrt(5))),
-		new Punkt(0, 0, 5/Math.sqrt(50-10*Math.sqrt(5))),
-		new Punkt(-Math.sqrt(2/(5-Math.sqrt(5))), 0, -1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt(Math.sqrt(2/(5-Math.sqrt(5))), 0, 1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt((1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*-1/2*/-0.5, -1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt((1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*1/2*/0.5, -1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt(-(1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*-1/2*/-0.5, 1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt(-(1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*1/2*/0.5, 1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt(-(-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*-1/2*/-0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5))), -1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt(-(-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*1/2*/0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5))), -1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt((-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*-1/2*/-0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5))), 1/Math.sqrt(10-2*Math.sqrt(5))),
-		new Punkt((-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), /*1/2*/0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5))), 1/Math.sqrt(10-2*Math.sqrt(5))) };
-		
+				
+		new Punkt(0, -5/Math.sqrt(50-10*Math.sqrt(5)), 0),
+		new Punkt(0, 5/Math.sqrt(50-10*Math.sqrt(5)), 0),
+		new Punkt(-Math.sqrt(2/(5-Math.sqrt(5))), (-1/Math.sqrt(10-2*Math.sqrt(5))), 0),
+		new Punkt(Math.sqrt(2/(5-Math.sqrt(5))), 1/Math.sqrt(10-2*Math.sqrt(5)), 0),
+		new Punkt((1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),-1/Math.sqrt(10-2*Math.sqrt(5)),  /*-1/2*/-0.5),
+		new Punkt((1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),-1/Math.sqrt(10-2*Math.sqrt(5)),  /*1/2*/0.5),
+		new Punkt(-(1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),1/Math.sqrt(10-2*Math.sqrt(5)),  /*-1/2*/-0.5),
+		new Punkt(-(1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),1/Math.sqrt(10-2*Math.sqrt(5)), /*1/2*/0.5),
+		new Punkt(-(-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), -1/Math.sqrt(10-2*Math.sqrt(5)),/*-1/2*/-0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5)))), 
+		new Punkt(-(-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),-1/Math.sqrt(10-2*Math.sqrt(5)) ,/*1/2*/0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5)))), 
+		new Punkt((-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))), 1/Math.sqrt(10-2*Math.sqrt(5)) ,/*-1/2*/-0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5)))),
+		new Punkt((-1+Math.sqrt(5))/(2*Math.sqrt(10-2*Math.sqrt(5))),1/Math.sqrt(10-2*Math.sqrt(5)) , /*1/2*/0.5*Math.sqrt((5+Math.sqrt(5))/(5-Math.sqrt(5)))) };
+				
 		for (Punkt p : eckpunkt) 
 			punkte.add(p);
 
-		/*kanten.add(new Kante(eckpunkt[2], eckpunkt[3]));
 		kanten.add(new Kante(eckpunkt[0], eckpunkt[2]));
-		kanten.add(new Kante(eckpunkt[3], eckpunkt[4]));
-		kanten.add(new Kante(eckpunkt[4], eckpunkt[5]));
-		kanten.add(new Kante(eckpunkt[6], eckpunkt[7]));
-		kanten.add(new Kante(eckpunkt[7], eckpunkt[8]));
-		kanten.add(new Kante(eckpunkt[8], eckpunkt[9]));
-		kanten.add(new Kante(eckpunkt[10], eckpunkt[11]));
-		kanten.add(new Kante(eckpunkt[11], eckpunkt[0]));*/
+		kanten.add(new Kante(eckpunkt[0], eckpunkt[4]));
+		kanten.add(new Kante(eckpunkt[0], eckpunkt[5]));
+		kanten.add(new Kante(eckpunkt[0], eckpunkt[8]));
+		kanten.add(new Kante(eckpunkt[0], eckpunkt[9]));
 		
-		kanten.add(new Kante(eckpunkt[0], eckpunkt[0]));
-		kanten.add(new Kante(eckpunkt[1], eckpunkt[1]));
-		kanten.add(new Kante(eckpunkt[2], eckpunkt[2]));
-		kanten.add(new Kante(eckpunkt[3], eckpunkt[3]));
-		kanten.add(new Kante(eckpunkt[4], eckpunkt[4]));
-		kanten.add(new Kante(eckpunkt[5], eckpunkt[5]));
-		kanten.add(new Kante(eckpunkt[6], eckpunkt[6]));
-		kanten.add(new Kante(eckpunkt[7], eckpunkt[7]));
-		kanten.add(new Kante(eckpunkt[8], eckpunkt[8]));
-		kanten.add(new Kante(eckpunkt[9], eckpunkt[9]));
-		kanten.add(new Kante(eckpunkt[10], eckpunkt[10]));
-		kanten.add(new Kante(eckpunkt[11], eckpunkt[11]));
+		kanten.add(new Kante(eckpunkt[1], eckpunkt[3]));
+		kanten.add(new Kante(eckpunkt[1], eckpunkt[6]));
+		kanten.add(new Kante(eckpunkt[1], eckpunkt[7]));
+		kanten.add(new Kante(eckpunkt[1], eckpunkt[10]));
+		kanten.add(new Kante(eckpunkt[1], eckpunkt[11]));
+		
+		kanten.add(new Kante(eckpunkt[2], eckpunkt[8]));
+		kanten.add(new Kante(eckpunkt[2], eckpunkt[9]));
+		kanten.add(new Kante(eckpunkt[4], eckpunkt[5]));
+		kanten.add(new Kante(eckpunkt[4], eckpunkt[8]));
+		kanten.add(new Kante(eckpunkt[5], eckpunkt[9]));
+		
+		kanten.add(new Kante(eckpunkt[6], eckpunkt[7]));
+		kanten.add(new Kante(eckpunkt[3], eckpunkt[10]));
+		kanten.add(new Kante(eckpunkt[6], eckpunkt[10]));
+		kanten.add(new Kante(eckpunkt[3], eckpunkt[11]));
+		kanten.add(new Kante(eckpunkt[7], eckpunkt[11]));
+		
+		kanten.add(new Kante(eckpunkt[2], eckpunkt[6]));
+		kanten.add(new Kante(eckpunkt[2], eckpunkt[7]));
+		kanten.add(new Kante(eckpunkt[7], eckpunkt[9]));
+		kanten.add(new Kante(eckpunkt[9], eckpunkt[11]));
+		kanten.add(new Kante(eckpunkt[5], eckpunkt[11]));
+		kanten.add(new Kante(eckpunkt[3], eckpunkt[5]));
+		kanten.add(new Kante(eckpunkt[3], eckpunkt[4]));
+		kanten.add(new Kante(eckpunkt[4], eckpunkt[10]));
+		kanten.add(new Kante(eckpunkt[8], eckpunkt[10]));
+		kanten.add(new Kante(eckpunkt[8], eckpunkt[6]));
 		
 	}
 }
